6012c423b13bff904ac5890e0617a3a8b60cfa58
Sreeram Jayan <62942671+sreeram91@users.noreply.github.com>
2025-01-01 17:08:44 +0530

    Create itemget_hardcoded.py

---
+++ itemget_hardcoded.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+zabbix_api_url = "http://65.2.153.81/zabbix/api_jsonrpc.php"
+username = "Admin"
+password = "zabbix"
+
+def authenticate(url, user, password):
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "user.login",
+        "params": {
+            "username": user,
+            "password": password
+        },
+        "id": 1
+    }
+    response = requests.post(url, json=payload)
+    response_data = response.json()
+
+    if "result" in response_data:
+        return response_data.get("result")  # Return the authentication token
+    else:
+        raise Exception(f"Failed to authenticate. Error: {response_data.get('error')}")
+    
+def get_hosts(url, token):
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json-rpc"
+    }
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "host.get",
+        "params": {
+            "output": [
+                "hostid",
+                "host"
+            ],
+            "selectInterfaces": ["interfaceid", "ip"]
+        },
+        "id": 2
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    response_data = response.json()
+
+    #rint(f"Response Data is: {response_data}")  # Debugging line
+
+    if "result" in response_data:
+        return response_data.get("result")
+    else:
+        raise Exception(f"Failed to retrieve hosts. Error: {response_data.get('error')}")
+
+def get_items(url, token):
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json-rpc"
+    }
+    
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "item.get",
+        "params": {
+            "output": "extend",
+            "hostids": "10650",
+            "with_triggers": True,
+            "search": {
+                "key_": "system.cpu"
+            },
+            "sortfield": "name"
+        },
+        "id": 1
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    response_data = response.json()
+
+   #print(f'Response data is: {response_data}') #Debugging line
+    
+    if 'result' in response_data:
+        return response_data.get('result')
+    else:
+        raise Exception(f"Failed to retrieve items: {response_data.get('error')}")
+
+def main():
+    try:
+        token = authenticate(zabbix_api_url, username, password)
+        print(f'Token Generated: {token}') #Debugging line
+
+        hosts = get_hosts(zabbix_api_url, token)
+
+        print(f'Hosts Details')
+        for host in hosts:
+            print(f"Host ID: {host['hostid']}, Host: {host['host']}")
+
+        items = get_items(zabbix_api_url, token)
+
+        print(f'Item Details')
+        for item in items:
+             print(f"Item ID: {item['itemid']}, Type: {item['type']}")
+    except Exception as e:
+        print(f'Error: {e}')
+
+if __name__ == '__main__':
+    main()
+
+    
